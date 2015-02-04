e0b239af7cd3506647b9e4fe04d108127b162065
王宇轩 <crnlmcn@gmail.com>
2015-02-04 18:12:02 +0800

    add support for spring spel

---
+++ config-toolkit-easyzk/src/main/java/com/dangdang/config/service/easyzk/support/spring/ConfigNodeIndexer.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 1999-2014 dangdang.com.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dangdang.config.service.easyzk.support.spring;
+
+import java.util.Properties;
+
+import com.dangdang.config.service.easyzk.ConfigNode;
+
+/**
+ * Support for spel, can use with @Value("#{configNode['key']}")
+ * 
+ * @author <a href="mailto:wangyuxuan@dangdang.com">Yuxuan Wang</a>
+ *
+ */
+public class ConfigNodeIndexer extends Properties {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param configNode
+	 */
+	public ConfigNodeIndexer(ConfigNode configNode) {
+		super();
+		this.putAll(configNode.exportProperties());
+	}
+
+}
