014eac40c0f546facf8d0eb4a8e658379510c7b9
yuxuanwang <crnlmcn@gmail.com>
2015-11-06 13:49:04 +0800

    fix local cache demo

--- config-toolkit-demo/src/main/java/com/dangdang/config/service/easyzk/demo/simple/LocalCacheCase.java
+++ config-toolkit-demo/src/main/java/com/dangdang/config/service/easyzk/demo/simple/LocalCacheCase.java
@@ -1,6 +1,6 @@
 package com.dangdang.config.service.easyzk.demo.simple;
 
-import com.dangdang.config.service.ConfigGroup;
+import com.dangdang.config.service.zookeeper.ConfigLocalCache;
 import com.dangdang.config.service.zookeeper.ZookeeperConfigGroup;
 import com.dangdang.config.service.zookeeper.ZookeeperConfigProfile;
 
@@ -14,10 +14,11 @@ public class LocalCacheCase {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ZookeeperConfigProfile profile = new ZookeeperConfigProfile("zk.host", "/projectx/modulex", true);
-		profile.setLocalCacheFolder("/your/local/config/folder");
-		
-		ConfigGroup dbConfigs = new ZookeeperConfigGroup(null, profile, "db");
+		String rootNode = "/projectx/modulex";
+		ZookeeperConfigProfile profile = new ZookeeperConfigProfile("zk.host", rootNode, true);
+
+		ZookeeperConfigGroup dbConfigs = new ZookeeperConfigGroup(null, profile, "db");
+		dbConfigs.setConfigLocalCache(new ConfigLocalCache("/your/local/config/folder", rootNode));
 	}
 
 }
