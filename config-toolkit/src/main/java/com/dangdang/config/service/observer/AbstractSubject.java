9a482f8ed52d415b77855efefb6acd112bcee7b5
yuxuanwang <crnlmcn@gmail.com>
2018-03-09 08:57:17 +0800

    simplify the structure

---
+++ config-toolkit/src/main/java/com/dangdang/config/service/observer/AbstractSubject.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 1999-2014 dangdang.com.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dangdang.config.service.observer;
+
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
+/**
+ * 主题通用实现
+ * 
+ * @author <a href="mailto:wangyuxuan@dangdang.com">Yuxuan Wang</a>
+ *
+ */
+public abstract class AbstractSubject implements ISubject {
+
+	/**
+	 * 观察者列表
+	 */
+	private final List<IObserver> watchers = Lists.newArrayList();
+
+	@Override
+	public void register(final IObserver watcher) {
+		watchers.add(Preconditions.checkNotNull(watcher));
+	}
+
+	@Override
+	public void notify(final String key, final String value) {
+		for (final IObserver watcher : watchers) {
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					watcher.notified(key, value);
+				}
+			}).start();
+		}
+	}
+
+}
---
+++ config-toolkit/src/main/java/com/dangdang/config/service/proxy/RefreshableProxy.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 1999-2014 dangdang.com.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dangdang.config.service.proxy;
+
+import java.lang.reflect.Method;
+
+import com.google.common.base.Preconditions;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+/**
+ * 可刷新代理对象的动态代理
+ * 
+ * @author <a href="mailto:wangyuxuan@dangdang.com">Yuxuan Wang</a>
+ *
+ * @param <T>
+ *            代理对象
+ */
+public class RefreshableProxy<T> implements MethodInterceptor {
+
+	private T target;
+	
+	private final T proxy;
+
+	@SuppressWarnings("unchecked")
+	public RefreshableProxy(final T target) {
+		super();
+		this.target = Preconditions.checkNotNull(target);
+		final Enhancer enhancer = new Enhancer();
+		enhancer.setSuperclass(this.target.getClass());
+		enhancer.setCallback(this);
+		proxy = (T) enhancer.create();
+	}
+
+	public void refresh(final T target) {
+		this.target = Preconditions.checkNotNull(target);
+	}
+
+	public T getInstance() {
+		return proxy;
+	}
+
+	@Override
+	public Object intercept(final Object obj, final Method method, final Object[] args, final MethodProxy proxy) throws Throwable {
+		return proxy.invoke(target, args);
+	}
+
+}
